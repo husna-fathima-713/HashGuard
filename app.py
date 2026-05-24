41a8fcf7990f434ddf13c62bb1fc62ffa358ab49
husna-fathima-713 <husnafathima_icb2023@ksit.edu.in>
2026-05-24 13:54:42 +0530

    Implemented continuous background integrity monitoring

--- app.py
+++ app.py
@@ -2,18 +2,38 @@ from flask import Flask, render_template
 
 from src.monitor import check_integrity
 
+import threading
+import time
+
 
 app = Flask(__name__)
+
+latest_alerts = []
+
+
+def background_monitor():
+
+    global latest_alerts
+
+    while True:
+
+        latest_alerts = check_integrity()
+
+        time.sleep(5)
 
 
 @app.route("/")
 def home():
 
-    alerts = check_integrity()
-
-    return render_template("index.html", alerts=alerts)
+    return render_template("index.html", alerts=latest_alerts)
 
 
 if __name__ == "__main__":
+
+    monitor_thread = threading.Thread(target=background_monitor)
+
+    monitor_thread.daemon = True
+
+    monitor_thread.start()
 
     app.run(debug=True)
